347d70873e21f5f9f36723ecd5fe6f5671c76230
Matt Mikolajczyk <mmikolajczyk@sqasquared.com>
2018-09-25 18:09:16 -0700

    first commit

---
+++ miko/boards/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+
+from .models import Board
+
+def home(request):
+	boards = Board.objects.all()
+	return render(request, 'home.html', {'boards': boards})
