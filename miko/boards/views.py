3aa7754274380a9d387d887de9d3fb44d3f2c8c5
Matt Mikolajczyk <mmikolajczyk@sqasquared.com>
2018-09-25 21:18:52 -0700

    updated 404 error

--- miko/boards/views.py
+++ miko/boards/views.py
@@ -7,3 +7,7 @@ from .models import Board
 def home(request):
 	boards = Board.objects.all()
 	return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+	board = get_object_or_404(Board, pk=pk)
+	return render(request, 'topics.html', {'board': board})
--- miko/miko/urls.py
+++ miko/miko/urls.py
@@ -20,5 +20,7 @@ from boards import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
-    url(r'^admin/', admin.site.urls),
+	url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+	url(r'^admin/', admin.site.urls),
+
 ]
